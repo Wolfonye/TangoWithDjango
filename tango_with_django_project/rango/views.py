87c0b083c0610ee00daef48852455f96958c17d0
philipp-bous <wolf1bous@googlemail.com>
2017-08-15 11:07:17 +0200

    User authentication update
    Exercises noch machen! UND rauskriegen, wie ich an das Userbild eines Users drankomme.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
 
 #Import needed models here------------------------------------------------------
 from rango.models import Category
@@ -191,19 +193,60 @@ def register(request):
             registered = True
 
         #Invalid form(s) - mistakes or something else?
-        #Print problems to the terminal.
-        #They will also be shown to the user.
+        # Print problems to the terminal.
+        # They will also be shown to the user.
         else:
             print user_form.errors, profile_form.errors
 
-    #Not a HTTP POST, so we render our form using two ModelForm instances
+    # Not a HTTP POST, so we render our form using two ModelForm instances
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    #Render the template debending on the context.
+    # Render the template debending on the context.
     return render(request,
                   'rango/register.html',
                   {'user_form':user_form,
                    'profile_form': profile_form,
                    'registered': registered})
+
+def user_login(request):
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        #Gather the username and password provided by the user.
+        #This information is obtained from the login form
+            #We use request.POST.get('<variable>') as opposed to request.POST['<variable>']
+            #because the request.POST.get('<variable>') returns None, if the value
+            #does not exist while request.POST['<variable>'] will raise key error exception
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        #Use Django's machinery to attempt to see if the username/password
+        #combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                # As inactive account was used - no logging in!
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, 
+        # hence the blank dictionary object.
+        return render(request, 'rango/login.html',{})
